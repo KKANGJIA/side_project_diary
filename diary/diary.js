0e5342ff3f072d54f6652e2fd36945dca7d6463d
rkdwldk24 <rkdwldk16@gmail.com>
2021-03-20 15:34:54 +0900

    Edit a border logic and add a prev next calender

--- diary/diary.js
+++ diary/diary.js
@@ -1,16 +1,76 @@
 "use strict"
 
+//추가할 내용 => *내용 작성*
+//*큰 화면일 때 일기장에 들어오면 맨 상단에 포커스가 되지않음*
+
+const body = document.querySelector('body');
 const dateInput = document.querySelector('.nav__date__input');
 const titleInput = document.querySelector('.nav__title__input');
 const containerText = document.querySelector('.container__text');
+const submitBtn = document.querySelector('.submitBtn');
+
+//1. 일기장이 열리면 바로 실행되어야 할 기능들
+(function onload() {
+    onAddMoodTag();
+    onAddWeatherTag();
+    document.querySelector('.nav__title__input').focus();
+    //*추가하기 => date 자동 설정하기*
+    body.scrollIntoView({behavior: "smooth", block: "start"});
+}());
+
+//2. 일기장 작성 후 마무리 기능
+(function onFinish() {
+    onAlertFisish(); //작성 (미)완료 메세지
+    //*날씨와 기분 선택한 것 모두 리셋하기*
+}());
+
+function onAlertFisish() {
+    submitBtn.addEventListener('click', () => {
+        if(containerText.value == '' || titleInput.value == '' || dateInput.value == '') {
+            alert(`아직 작성하지 않았습니다 :(`);
+            return; //미작성 시 아무 것도 리턴하지 않기
+        }
+        if (containerText.value !== '' && titleInput.value !== '' && dateInput.value !== '') {
+            alert(`저장되었습니다 :)`);
+            onAddList(); //저장 후 리스트 추가하기
+
+            //기록 후에 전체 칸 초기화 및 제목에 포커스 잡아주기
+            dateInput.focus();
+            titleInput.value = '';
+            containerText.value = '';
+            dateInput.value = '';
+            body.scrollIntoView({behavior: "smooth", block: "start"});
+        }
+    })
+};
+
+function onAddList() {
+    const title = document.createElement('h1');
+    title.textContent = `작성된 일기장 리스트`;
+    title.style.fontSize = "2em";
+    title.style.marginLeft = "50px";
+
+    const list = document.createElement('div');
+    list.setAttribute('class', 'titleList');
+    list.textContent = `Title: ${titleInput.value}`;
+    list.style.border = '3px solid black';
+    list.style.padding = '10px 10px';
+    list.style.margin = '10px 0px';
+    list.style.width = '100%';
+    list.style.height = 'auto';
+
+    document.body.appendChild(title);
+    document.body.appendChild(list);
+}
+
 
 //기분 부분에 태그 추가하기
-(function onAddMoodTag() {
+function onAddMoodTag() {
     document.querySelector('.mood__imgs').innerHTML =
-    `<img class="mood__img" src="../img/happy.JPG"/>
-    <img class="mood__img" src="../img/grey.JPG"/>
-    <img class="mood__img" src="../img/question.JPG"/>
-    <img class="mood__img" src="../img/mad.JPG"/>`;
+    `<img id="1" class="mood__img" src="../img/happy.JPG"/>
+    <img  id="2" class="mood__img" src="../img/question.JPG"/>
+    <img  id="3" class="mood__img" src="../img/grey.JPG"/>
+    <img  id="4" class="mood__img" src="../img/mad.JPG"/>`;
 
     document.querySelector('.mood__des').innerHTML =
     `<span>행복</span>
@@ -18,10 +78,10 @@ const containerText = document.querySelector('.container__text');
     <span>의문</span>
     <span>분노</span>
     `;
-}());
+};
 
 //날씨 부분에 태그 추가하기
-(function onAddWeatherTag() {
+function onAddWeatherTag() {
     document.querySelector('.weather__imgs').innerHTML =
     `<img class="weather__img" src="../img/sun1.JPG"/>
     <img class="weather__img" src="../img/rain.JPG"/>
@@ -38,16 +98,36 @@ const containerText = document.querySelector('.container__text');
     <span>번개</span>
     <span>밤</span>
     `;
-}());
+};
 
-//순차적으로 focus 적용하기
-(function onFocus() {
-    document.querySelector('.nav__date__input').focus();
-        if(titleInput === '') {
-            document.querySelector('.nav__title__input').focus();
-            if(titleInput === '') {
-                document.querySelector('.container__text').focus();
-            }
-        }
-}());
+
+//이미지 클릭하면 경계선 지정하기
+//document.getElementById는 단일 DOM 객체를 가져오지만,
+//document.querySelectorAll은 NodeCollection을 가져오기 때문
+//배열이 아니라서 addEventListener를 바로 등록할 수 없다
+//따라서, forEach를 이용해서 각각 이벤트를 등록해준다
+let mood__img = document.querySelectorAll('.mood__img');
+// console.log(mood__img); //nodeList로 출력
+mood__img.forEach(function(moodImg) { //태그를 하나씩 돌면서 인자로 넘겨받은 태그에
+    moodImg.addEventListener('click', changeBorderToMood); //각각 클릭이벤트를 적용하기
+
+    function changeBorderToMood() {
+       moodImg.style.border = "5px solid #000000"; //보더 적용하는 함수
+    };
+});
+
+let weather__img = document.querySelectorAll('.weather__img');
+// console.log(weather__img); //nodeList로 출력
+weather__img.forEach(function(weatherImg) {
+    weatherImg.addEventListener('click', changeBorderToWeather);
+
+    function changeBorderToWeather() {
+        weatherImg.style.border = "5px solid #000000";
+    };    
+});
+
+
+
+
+
 
--- index/index.js
+++ index/index.js
@@ -1,50 +1,94 @@
 "use strict"
 
-const today = new Date();
-const todayYear = today.getFullYear();
-const todayMonth = today.getMonth();
+const nextBtn = document.querySelector('.calender__btn-next');
+const prevBtn = document.querySelector('.calender__btn-prev');
 
-document.querySelector('.year-month').innerHTML =
-`${todayYear}년 ${todayMonth}월`;
+let today = new Date();
+let todayYear = today.getFullYear();
+let todayMonth = today.getMonth();
 
-const prevLastDate = new Date(todayYear, todayMonth, 0); //이전 달의 마지막 날짜 구하기
-// console.log(prevLastDate); //Sun Feb 28 2021 00:00:00 GMT+0900 (대한민국 표준시)
-const thisLastDate = new Date(todayYear, todayMonth + 1, 0); // 이번 달의 마지막 날짜 구하기
+//이번 달 달력 로드
+(function onCreateThisMonth() {
+    document.querySelector('.year-month').innerHTML =
+    `${todayYear}년 ${todayMonth+1}월`;
+    onCreateCalender();
+}());
 
-const PLDate = prevLastDate.getDate(); //28
-const TLDate = thisLastDate.getDate(); //31
-const PLDay = prevLastDate.getDay(); //0, 즉, 일요일
-const TLDay = thisLastDate.getDay(); //3, 즉, 수요일
+//다음 달 달력 로드
+(function onGoNextMonth() {
+    nextBtn.addEventListener('click', () => {
+        // todayMonth > 13 ? todayMonth++ :  todayYear + 1;
+        if(todayMonth < 11) {
+            todayMonth++;
+        } else if (todayMonth >= 11) {
+            todayYear++;
+            todayMonth = 0;
+        }
+        document.querySelector('.year-month').innerHTML =
+        `${todayYear}년 ${todayMonth+1}월`;
+        onCreateCalender();
+    })
 
-const prevDates = []; // 이전 달의 날짜 배열 생성
-const thisDates = []; // 이번 달의 날짜 배열 생성
-const nextDates = []; // 다음 달의 날짜 배열 생성
+}());
 
-if( PLDay !== 6 ) {
-    for(let i = 0; i <= PLDay; i++) {
-        prevDates.unshift(PLDate - i); // 이전 달의 날짜를 뽑기
-        // console.log(prevDates);
+//이전 달 달력 로드
+(function onGoPrevMonth() {
+    prevBtn.addEventListener('click', () => {
+        if(todayMonth < 1) {
+            todayYear--;
+            todayMonth = 11;
+        } else {
+            todayMonth--;
+        }
+        document.querySelector('.year-month').innerHTML =
+            `${todayYear}년 ${todayMonth+1}월`;
+        onCreateCalender();
+    })
+}());
+
+//달력의 일자를 생성하는 메서드
+function onCreateCalender() {
+    const prevLastDate = new Date(todayYear, todayMonth, 0); //이전 달의 마지막 날짜 구하기
+    // console.log(prevLastDate); //Sun Feb 28 2021 00:00:00 GMT+0900 (대한민국 표준시)
+    const thisLastDate = new Date(todayYear, todayMonth + 1, 0); // 이번 달의 마지막 날짜 구하기
+    
+    const PLDate = prevLastDate.getDate(); //28
+    const TLDate = thisLastDate.getDate(); //31
+    const PLDay = prevLastDate.getDay(); //0, 즉, 일요일
+    const TLDay = thisLastDate.getDay(); //3, 즉, 수요일
+    
+    const prevDates = []; // 이전 달의 날짜 배열 생성
+    const thisDates = []; // 이번 달의 날짜 배열 생성
+    const nextDates = []; // 다음 달의 날짜 배열 생성
+    
+    if( PLDay !== 6 ) {
+        for(let i = 0; i <= PLDay; i++) {
+            prevDates.unshift(PLDate - i); // 이전 달의 날짜를 뽑기
+            // console.log(prevDates);
+        }
+        
+        for(let i = 1; i <= 6 - TLDay; i++) {
+            // console.log(i); //다음 달의 날짜를 뽑기
+            nextDates.push(i);
+            // console.log(nextDates);
+        }
+    
+        for(let i = 1; i <= TLDate; i++ ) {
+            thisDates.push(i); //이번 달 날짜 전부를 뽑기
+            // console.log(thisDates);
+        }
+    
+        const dates = prevDates.concat(thisDates, nextDates);
+    
+        dates.forEach((date, i) => {
+            dates[i] = `<a class="date" href="../diary/diary.html"><div>${date}</div></a>`;
+          })
+    
+        document.querySelector('.dates').innerHTML = dates.join('');
     }
     
-    for(let i = 1; i <= 6 - TLDay; i++) {
-        // console.log(i); //다음 달의 날짜를 뽑기
-        nextDates.push(i);
-        // console.log(nextDates);
-    }
-
-    for(let i = 1; i <= TLDate; i++ ) {
-        thisDates.push(i); //이번 달 날짜 전부를 뽑기
-        // console.log(thisDates);
-    }
-
-    const dates = prevDates.concat(thisDates, nextDates);
-
-    dates.forEach((date, i) => {
-        dates[i] = `<a class="date" href="../diary/diary.html"><div>${date}</div></a>`;
-      })
-
-    document.querySelector('.dates').innerHTML = dates.join('');
 }
+
 
 
 
